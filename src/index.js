6d959cedb402ebd094024c45494455c443bdbab7
Darshan Datta <darshandatta.10@gmail.com>
2019-05-09 16:27:16 +0530

    added html boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './scss/styles.scss'
+import api from './api'
+
+const test = async () => {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${api}`, {})
+    if (response.status === 200) {
+        return response.json()
+    } else {
+        throw new Error()
+    }
+}
+// test().then((data) => {
+//     console.log(data)
+// }).catch((e) => {
+//     console.log(e)
+// })
